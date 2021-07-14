f8845a56e708a1c79e90bc416fb7becd047b04d9
Seb <sebwiggins@me.com>
2021-07-14 09:51:37 +0100

    working search bar display

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 // import { greet } from "./utils/greet";
 import episodes from "./episodes2.json"
 import Episode from "./components/Episode"
+import {useState} from 'react'
 
 function App(): JSX.Element {
-  const episodeArray = episodes
+  const [searchTerm, setSearchTerm] = useState("")
+ 
+  const filteredEpisodes = episodes.filter((val) => 
+  val.name.toLowerCase().includes(searchTerm.toLowerCase())
+  ||val.summary.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <>
-      {episodeArray.map(ep=><Episode
-      key={ep.name}
-      name={ep.name}
-      season={ep.season}
-      number={ep.number}
-      image={ep.image}
-      summary={ep.summary}
-      url={ep.url}
-      />)}
+    <div>
+     <input type="text" 
+     placeholder="Search..." 
+     onChange={(event) => 
+     {setSearchTerm(event.target.value)}}/>
+     <p>Showing {filteredEpisodes.length}/{episodes.length} episodes</p>
+    </div>
+
+    {filteredEpisodes.map(ep=><Episode
+    key={ep.name}
+    name={ep.name}
+    season={ep.season}
+    number={ep.number}
+    image={ep.image}
+    summary={ep.summary}
+    url={ep.url}
+    />)}
     </>
   );
 }
